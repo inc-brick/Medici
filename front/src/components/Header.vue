60357e55d990a231366a06f3255da0a630043919
Kenta Takimura <38579991+kennyTakimura@users.noreply.github.com>
2019-09-19 00:51:02 +0900

    アーティスト画面全体感の実装 (#6)

---
+++ front/src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <img src="./../assets/brick-logo.png" style="height: 30px; width: 60px">
+<!--  <el-header :height=height class="header_title">-->
+<!--    <el-image-->
+<!--      style="width: 100px; height: 100px"-->
+<!--      :src="url"-->
+<!--      :fit="fit"></el-image>-->
+<!--  </el-header>-->
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data() {
+      return {
+          url: "/Users/kentatakimur/dev/Medici/front/src/assets/brick-logo.png",
+          fit: "contain",
+          height: "30px"
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.header_title {
+  font-size: 18px;
+  font-weight: bold;
+}
+</style>
---
+++ front/src/pages/Artist.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="artist">
+    <h3>山本　捷平</h3>
+    <h3>Shohei Yamamoto</h3>
+    <WorkView :urls="urls"></WorkView>
+    <h2>About</h2>
+    <p>ここのそのアーティストについての内容</p>
+    <h2>Upcoming Events</h2>
+    <EventView :urls="urls"></EventView>
+    <h2>Contacts</h2>
+    <i class="el-icon-shopping-cart-2"></i>
+    <h2>Media</h2>
+    <MediaView :urls="urls"></MediaView>
+  </div>
+</template>
+
+<script>
+import WorkView from "../components/WorkView";
+import EventView from "../components/EventView";
+import MediaView from "../components/MediaView";
+
+export default {
+  name: "artist",
+  components: {WorkView, EventView, MediaView},
+  data () {
+      return {
+          msg: 'Welcome to Your Vue.js App',
+          urls: ['https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg']
+      }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
