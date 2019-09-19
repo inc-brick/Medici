c67d611816b92ccbd6f27f0776de8a0b3d015427
Kenta Takimura <kennyTakimura>
2019-09-20 01:31:11 +0900

    見た目部分の実装

---
+++ front/src/pages/Result.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="result">
+    <p>お問い合わせありがとうございます！</p>
+    <p>担当者からのご連絡お待ちください。</p>
+    <div class="back-to-artist-button">
+      <router-link to="/"><el-button type="Info">Back to Artist</el-button></router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "result"
+}
+</script>
+
+<style scoped>
+.result{
+  margin-top: 12rem;
+  font-weight: bold;
+}
+.back-to-artist-button {
+  text-align: center;
+  margin-top: 32px;
+}
+</style>
--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Artist from '@/pages/Artist'
+import Contact from "../pages/Contact";
+import Result from "../pages/Result";
 
 Vue.use(Router)
 
@@ -10,6 +12,16 @@ export default new Router({
       path: '/',
       name: 'Artist',
       component: Artist
+    },
+    {
+      path: '/contact',
+      name: 'Contact',
+      component: Contact
+    },
+    {
+      path: '/result',
+      name: 'Result',
+      component: Result
     }
   ]
 })
