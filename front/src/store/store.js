9eb01fa3e5e59ca4020e0d9eb28ad44a2a220618
Kenta Takimura <38579991+kennyTakimura@users.noreply.github.com>
2019-09-22 14:28:32 +0900

    connectorの基本実装 (#11)

---
+++ front/src/store/store.js
@@ -0,0 +1,17 @@
+import Vue from 'vue';
+import Vuex from 'vuex'
+import Router from "vue-router";
+
+Vue.use(Vuex)
+
+export default new Router({
+  state: {
+    artistId: 0
+  },
+  mutations: {
+
+  },
+  actions: {
+
+  }
+})
