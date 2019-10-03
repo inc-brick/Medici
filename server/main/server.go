51731e175dec4005b5d74d2ca5e7edf279b4cc4c
Kenta Takimura <38579991+kennyTakimura@users.noreply.github.com>
2019-10-04 01:01:49 +0900

    EventWatchのソースをコピー (#16)

---
+++ server/main/server.go
@@ -0,0 +1,331 @@
+package main
+
+import (
+"encoding/json"
+"fmt"
+_ "github.com/go-sql-driver/mysql"
+"github.com/gorilla/sessions"
+"github.com/jmoiron/sqlx"
+"github.com/labstack/echo"
+"github.com/labstack/echo-contrib/session"
+"github.com/labstack/gommon/log"
+"golang.org/x/crypto/bcrypt"
+"io/ioutil"
+"net/http"
+"os"
+"strconv"
+"strings"
+"time"
+)
+
+var (
+	db *sqlx.DB
+	err error
+)
+
+type SignupParam struct {
+	UserName string `json:"username"`
+	Password string `json:"password"`
+	PasswordConfirmation string `json:"password_confirmation"`
+}
+
+type LoginParam struct {
+	UserName string `json:"username"`
+	Password string `json:"password"`
+}
+
+type EventsParam struct {
+	Language string `json:"language"`
+	Area string `json:"area"`
+}
+
+type EventApiResponse struct {
+	ResultsReturned int `json:"results_returned"`
+	ResultsAvailable int `json:"results_available"`
+	ResultsStart int `json:"results_start"`
+	Events []Event `json:"events"`
+}
+
+type Event struct {
+	EventId int `json:"event_id"`
+	Title string `json:"title"`
+	Catch string `json:"catch"`
+	Description string `json:"description"`
+	EventUrl string `json:"event_url"`
+	HashTag string `json:"hash_tag"`
+	StartedAt string `json:"started_at"`
+	EndedAt string `json:"ended_at"`
+	Limit int `json:"limit"`
+	EventType string `json:"event_type"`
+	Series Series `json:"series"`
+	Address string `json:"address"`
+	Place string `json:"place"`
+	Lat string `json:"lat"`
+	Lon string `json:"lon"`
+	OwnerId int `json:"owner_id"`
+	OwnerNickname string `json:"owner_nickname"`
+	OwnerDisplayName string `json:"owner_display_name"`
+	Accepted int `json:"accepted"`
+	Waiting int `json:"waiting"`
+	UpdatedAt string `json:"updated_at"`
+}
+
+type Series struct {
+	Id int `json:"id"`
+	Title string `json:"title"`
+	Url string `json:"url"`
+}
+type User struct {
+	Id int64 `json:"user_id" db:"id"`
+	UserName string `json:"username" db:"username"`
+	Password string `json:"-" db:"digested_password"`
+	CreatedAt time.Time `json:"-" db:"created_at"`
+	UpdatedAt time.Time `json:"-" db:"updated_at"`
+}
+
+type ComArtistPrivateInfo struct {
+	
+}
+
+type ArtArtistEvent struct {
+	EventId int `json:"event_id" db:"EVENT_ID"`
+	ArtistId int `json:"artist_id" db:"ARTIST_ID"`
+	StartDate time.Time `json:"start_date" db:"START_DATE"`
+	EndDate time.Time `json:"end_date" db:"END_DATE"`
+	EventNameJA string `json:"-" db:"EVENT_NAME_JA"`
+	EventNameEN string `json:"-" db:"EVENT_NAME_EN"`
+	EventDescriptionJA string `json:"-" db:"EVENT_DESCRIPTION_JA"`
+	EventDescriptionEN string `json:"-" db:"EVENT_DESCRIPTION_EN"`
+	EventImagePath string `json:"-" db:"EVENT_IMAGE_PATH"`
+	EventStartAt time.Time `json:"event_start_at" db:"EVENT_START_AT"`
+	EventEndAt time.Time `json:"event_end_at" db:"EVENT_END_AT"`
+	EventPhone int `json:"-" db:"EVENT_PHONE"`
+	EventMailAddress string `json:"-" db:"EVENT_MAIL_ADDRESS"`
+	EventUrl string `json:"-" db:"EVENT_URL"`
+}
+
+func init() {
+}
+
+func signup(c echo.Context) error {
+	log.Info("singup is called")
+	req := new(SignupParam)
+	if err := c.Bind(req); err != nil {
+		return err
+	}
+	// validation
+	if req.Password != req.PasswordConfirmation {
+		return c.JSON(http.StatusBadRequest, "password is not matched")
+	}
+	if len(req.Password) < 8 {
+		return c.JSON(http.StatusBadRequest, "password length less than 8")
+	}
+	log.Info("password validation: OK")
+	// store
+	digestedPassword, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
+	if err != nil {
+		log.Errorf("Failed to encrypt password. %s", err.Error())
+	}
+	log.Info("password encryption: OK")
+	tx, err := db.Begin()
+	if err != nil {
+		log.Errorf("Failed to begin transaction. %s", err.Error())
+	}
+	log.Info("transaction begin: OK")
+	res, err := tx.Exec("INSERT INTO `users` (`id`, `username`, `digested_password`, `created_at`, `updated_at`) VALUES (null, ?,?,?,?)",
+		req.UserName,
+		digestedPassword,
+		time.Now(),
+		time.Now())
+	if err != nil {
+		log.Errorf("Failed to execute query. %s", err.Error())
+	}
+	err = tx.Commit()
+	if err != nil {
+		log.Errorf("Failed to commit. %s", err.Error())
+	}
+	log.Info("db execution: OK %s", res)
+
+	// session
+	lastInsertedId, _ := res.LastInsertId()
+	saveSession(lastInsertedId, req.UserName, true, c)
+	// select user
+	var user User
+	if err := db.Get(&user,"SELECT * FROM `users` WHERE `id` = ?", lastInsertedId); err != nil {
+		log.Errorf("Failed to execute query. %s", err.Error())
+	}
+	return c.JSON(http.StatusOK, user)
+}
+
+func login(c echo.Context) error {
+	log.Info("login is called")
+	req := new(LoginParam)
+	if err := c.Bind(req); err != nil {
+		return err
+	}
+	var user User
+	if err := db.Get(&user, "SELECT * FROM `users` WHERE `username` = ?", req.UserName); err != nil {
+		log.Errorf("Failed to execute query. %s", err.Error())
+		return c.JSON(http.StatusUnauthorized, "Can not find user")
+	}
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(req.Password)); err != nil{
+		log.Info(err)
+		return c.JSON(http.StatusUnauthorized, "password doesn't match")
+	}
+	// session
+	saveSession(user.Id, user.UserName, true, c)
+	return c.JSON(http.StatusOK, user)
+}
+
+func logout(c echo.Context) error {
+	authenticated, err := authenticated(c)
+	if authenticated == 0 {
+		return err
+	}
+	sess, _ := session.Get("EVENTWATCH_SESSION", c)
+	saveSession(0, "", false, c)
+	if err := sess.Save(c.Request(), c.Response()); err != nil {
+		log.Errorf("Can not save the session. %s", err.Error())
+	}
+	return c.JSON(http.StatusOK, nil)
+}
+
+func test(c echo.Context) error {
+	userId, err := authenticated(c)
+	if userId == 0 {
+		return err
+	}
+	req := new(EventsParam)
+	if err := c.Bind(req); err != nil {
+		return err
+	}
+	url := "https://connpass.com/api/v1/event/"
+	client := &http.Client{}
+	apiReq, err := http.NewRequest("GET", url+buildQueryParam(req.Language, req.Area), nil)
+	if err != nil {
+		log.Error(err)
+		return err
+	}
+	apiRes, err := client.Do(apiReq)
+	if err != nil {
+		log.Info(err)
+		return err
+	}
+	defer apiRes.Body.Close()
+
+	if apiRes.StatusCode != http.StatusOK {
+		log.Info(apiRes)
+		return c.JSON(http.StatusInternalServerError, nil)
+	}
+	body, err := ioutil.ReadAll(apiRes.Body)
+	if err != nil {
+		log.Errorf("Can not read response body. %s", err.Error())
+	}
+
+	var eventApiResponse EventApiResponse
+	err = json.Unmarshal(body, &eventApiResponse)
+	if err != nil {
+		log.Errorf("Could not unmarshal response body. %s", err.Error())
+	}
+	var events []Event
+	for _, v := range eventApiResponse.Events {
+		if strings.Contains(v.Address, req.Area) {
+			events = append(events, v)
+		}
+	}
+	return c.JSON(http.StatusOK, events)
+}
+
+func fetchArtistInfo(c echo.Context) error {
+	log.Info("login is called")
+	artistId := c.Param("artistId")
+	var user User
+	if err := db.Get(&user, "SELECT * FROM `users` WHERE `username` = ?", req.UserName); err != nil {
+		log.Errorf("Failed to execute query. %s", err.Error())
+		return c.JSON(http.StatusUnauthorized, "Can not find user")
+	}
+	return c.JSON(http.StatusOK, artistId)
+}
+
+func main() {
+	dbHost := os.Getenv("MEDICI _MYSQL_HOST")
+	if dbHost == "" {
+		dbHost = "127.0.0.1"
+	}
+	dbPort := os.Getenv("MEDICI_MYSQL_PORT")
+	if dbPort == "" {
+		dbPort = "3306"
+	}
+	_, err := strconv.Atoi(dbPort)
+	if err != nil {
+		log.Fatalf("failed to read DB port number from an environment variable MYSQL_PORT.\nError: %s", err.Error())
+	}
+	dbUserName := os.Getenv("MEDICI_MYSQL_USERNAME")
+	if dbUserName == "" {
+		dbUserName = "brickdev01"
+	}
+	dbPassword := os.Getenv("MEDICI_MYSQL_PASSWORD")
+	if dbPassword == "" {
+		dbPassword = "brickdev012019"
+	}
+	dbName := os.Getenv("MEDICI_MYSQL_DBNAME")
+	if dbName == "" {
+		dbName = "medici"
+	}
+	datasource := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=true&loc=Local", dbUserName, dbPassword, dbHost, dbPort, dbName)
+	db, err = sqlx.Open("mysql", datasource)
+	if err != nil {
+		log.Fatalf("failed to connect to DB: %s.", err.Error())
+	}
+	defer db.Close()
+	e := echo.New()
+	e.Use(session.Middleware(sessions.NewCookieStore([]byte("MEDICI_COOKIE_STORE_KEY"))))
+	e.POST("/signup", signup)
+	e.POST("/login", login)
+	e.POST("/logout", logout)
+	e.POST("/events", test)
+	e.POST("/artist/:artistId", fetchArtistInfo)
+	e.Logger.Fatal(e.Start(":8000"))
+}
+
+func authenticated(c echo.Context) (interface{}, error) {
+	sess, _ := session.Get("MEDICI_SESSION", c)
+	b, _ := sess.Values["authenticated"]
+	if b != true {
+		log.Info("User not logged in")
+		return 0, c.JSON(http.StatusUnauthorized, "please sign up or login at first")
+	}
+	userId, ok := sess.Values["userId"]
+	if !ok {
+		log.Error("Can not get userId from session store.")
+		return 0, c.JSON(http.StatusUnauthorized, "please sign up or login at first")
+	}
+	return userId, nil
+}
+
+func saveSession(userId int64, username string, authenticated bool, c echo.Context) {
+	// session
+	sess, _ := session.Get("MEDICI_SESSION", c)
+	sess.Options = &sessions.Options{
+		Path:	"/",
+		MaxAge:	3600,
+		HttpOnly: true,
+	}
+	sess.Values["userId"] = userId
+	sess.Values["username"] = username
+	sess.Values["authenticated"] = authenticated
+	if err := sess.Save(c.Request(), c.Response()); err != nil {
+		log.Errorf("Can not save the session. %s", err.Error())
+	}
+}
+
+func buildQueryParam(language, area string) string {
+	s := ""
+	if len(language) != 0 {
+		s = s + "?keyword=" + language
+	}
+	if len(area) != 0 {
+		s = s + "&keyword=" + area
+	}
+	return s
+}
