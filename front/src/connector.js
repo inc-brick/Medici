b5d329b8bff7dd79aff50a7267375fc883615c87
Kenta Takimura <38579991+kennyTakimura@users.noreply.github.com>
2019-10-31 01:02:03 +0900

    Googleフォームと連携 (#29)

--- front/src/connector.js
+++ front/src/connector.js
@@ -21,6 +21,21 @@ const connector = {
   },
   async postContactsInfo() {
     await axios.post("http://localhost:8080", {})
+  },
+  async postGform(data) {
+    await axios.post(
+      "https://docs.google.com/forms/u/1/d/e/1FAIpQLSc10-M1uZi5jD2jmyK_ICom4KipjEWXv6O6xHqTQq6vyvO_hg/formResponse",
+      {
+        "entry.1239827993": data['name'],
+        "entry.1682303839": data['email'],
+        "entry.1390168152": data['phone']
+    },
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          "Access-Control-Allow-Origin": ""
+        }
+      })
   }
 }
 
