07f0c23f50593b6c46a20cec655443dbf3fd890b
Kenta Takimura <38579991+kennyTakimura@users.noreply.github.com>
2019-11-06 23:46:23 +0900

    サーバーを介してgoogle formにpostする (#36)

--- front/src/connector.js
+++ front/src/connector.js
@@ -23,17 +23,13 @@ const connector = {
     await axios.post("http://localhost:8080", {})
   },
   async postGform(data) {
-    await axios.post(
-      "https://docs.google.com/forms/u/1/d/e/1FAIpQLSc10-M1uZi5jD2jmyK_ICom4KipjEWXv6O6xHqTQq6vyvO_hg/formResponse",
-      {
-        "entry.1239827993": data['name'],
-        "entry.1682303839": data['email'],
-        "entry.1390168152": data['phone']
-    },
+    return await axios.post(
+      "http://localhost:8000/post/contact",
+      data,
       {
         headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-          "Access-Control-Allow-Origin": ""
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*"
         }
       })
   }
