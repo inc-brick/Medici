5c0334a569289a60151e26f45170997318b56670
Kenta Takimura <kennyTakimura>
2019-11-06 23:07:59 +0900

    連絡方法をstringに変更

--- server/main/request/request.go
+++ server/main/request/request.go
@@ -3,7 +3,7 @@ package request
 type GoogleFormParam struct {
 	Name string `json:"name"`
 	WorkName string `json:"work"`
-	Method int `json:"method"`
+	Method string `json:"method"`
 	Email string `json:"email"`
 	Phone string `json:"phone"`
 }
