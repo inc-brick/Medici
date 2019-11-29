618375176e138520beb46245bf7532f5c3e4160c
corentytakeshi <takerudrums@gmail.com>
2019-11-29 19:44:06 +0900

    modify
    #50 #51

--- server/main/server.go
+++ server/main/server.go
@@ -344,7 +344,7 @@ func main() {
 	e := echo.New()
 	e.Use(session.Middleware(sessions.NewCookieStore([]byte("MEDICI_COOKIE_STORE_KEY"))))
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{"http://app-brick.com"},
+		AllowOrigins: []string{"https://app-brick.com"},
 		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAccessControlAllowOrigin},
 	}))
 	e.POST("/signup", signup)
