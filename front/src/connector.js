2669c7521cc1d847a3d64238ff705dd536cbdffd
corentytakeshi <takerudrums@gmail.com>
2019-11-29 20:06:24 +0900

    modify
    #46

--- front/src/connector.js
+++ front/src/connector.js
@@ -24,7 +24,7 @@ const connector = {
   },
   async postGform(data) {
     let res = await axios.post(
-      "https://app-brick.com:8000/post/contact",
+      "http://app-brick.com:8000/post/contact",
       data,
       {
         headers: {
