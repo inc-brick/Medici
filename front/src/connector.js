0327b43f2ac42f659285b8904dcd833ad56bf606
kennyTakimura <kennytakimura@gmail.com>
2020-02-07 00:10:34 +0900

    環境によって接続先を変える修正の調査結果

--- front/src/connector.js
+++ front/src/connector.js
@@ -6,14 +6,12 @@ const ENV = {
   PRODUCTION: "production"
 }
 
-if (process.env.NODE_ENV === ENV.MOCK) {
-
-}
-
-
 const connector = {
   // TODO(takimura): このままだとCORSのエラーになる。サーバー側の修正が必要
   async getArtistData() {
+    if (process.env.NODE_ENV === ENV.DEVELOPMENT) {
+      console.log("This is displayed only when you connected to the dev server")
+    }
     // await axios.get("http://localhost:8080", {})
   },
   async postArtistWork() {
